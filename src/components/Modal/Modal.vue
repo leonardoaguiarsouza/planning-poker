659608fa2bda58c7a4f69a3a36eefee6940211a9
Leonardo Bruno de Aguiar de Souza <leonardo.aguiar@compasso.com.br>
2023-11-01 18:35:22 -0300

    adjustments

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <dialog :open="isOpen">
+    <article>
+      <a
+        aria-label="Close"
+        class="close"
+        @click="closeModal()"
+      >
+      </a>
+      <h3>{{ title }}</h3>
+      <slot></slot>
+    </article>
+  </dialog>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  isOpen: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const isOpen = ref(true);
+
+const closeModal = () => {
+    isOpen.value = false;
+}
+</script>
+
+<style scoped>
+article {
+  max-width: 500px;
+  width: 100%;
+}
+
+a {
+    cursor: pointer;
+}
+
+footer {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
